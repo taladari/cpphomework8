8c8147851391df1413b13d49d2001f7d6ff1d141
Tal Adari <tal0392@gmail.com>
2018-04-28 15:36:31 +0300

    added [] operator

--- Board.cpp
+++ Board.cpp
@@ -1,21 +1,22 @@
 #include "Board.h"
 
-
+// i = row * boardSize + col
 
 Board::Board(int boardSize)
 {
 	this->_boardSize = boardSize;
-	//for (size_t i = 0; i < _boardSize; i++)
-	//{
-	//	vector<char> temp;
-	//	for (size_t j = 0; j < _boardSize; j++)
-	//	{
-	//		temp.push_back('.');
-	//	}
-	//	_board.push_back(temp);
-	//}
 	vector<char> temp(boardSize*boardSize, '.');
 	_board = temp;
+}
+
+char Board::operator[](boardIndex index) const
+{
+	return _board[index.i * _boardSize + index.j];
+}
+
+char & Board::operator[](boardIndex index)
+{
+	return (*this)[index];
 }
 
 ostream & operator<<(ostream & os, const Board & board)
